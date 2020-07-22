8be0c9e574795fcf0cf839f58953afc794e8d6a5
Ludovic Dureau <ludodureau@gmail.com>
2020-07-22 13:36:17 +0200

    entites redone

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -18,8 +18,25 @@ class Admin
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $email;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
     }
 }
